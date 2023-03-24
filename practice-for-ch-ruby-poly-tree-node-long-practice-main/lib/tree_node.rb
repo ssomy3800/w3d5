7785ba753034392150f11de269471dd4556aebca
Merve Dogan-Espaillat <1mervedogan@gmail.com>
2023-03-24 14:39:22 -0400

    finished initialize and parent=

--- practice-for-ch-ruby-poly-tree-node-long-practice-main/lib/tree_node.rb
+++ practice-for-ch-ruby-poly-tree-node-long-practice-main/lib/tree_node.rb
@@ -1,3 +1,35 @@
 class PolyTreeNode
-  
+  attr_reader :value, :parent, :children
+  attr_writer :children
+    
+    def initialize(value)
+        @value = value
+        @parent = nil
+        @children = []
+    end
+
+    def parent=(node)
+        @parent = node
+        if node != nil
+            if !node.children.include?(self)  
+            node.children << self
+             end
+        end
+    end
+
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 end
