a3e95471dc5db793882fadb5c42bf60eed870d5e
ssomy3800 <ssomy3800@gmail.com>
2023-03-24 12:24:36 -0700

    finished everything until dfs

--- practice-for-ch-ruby-poly-tree-node-long-practice-main/lib/tree_node.rb
+++ practice-for-ch-ruby-poly-tree-node-long-practice-main/lib/tree_node.rb
@@ -9,12 +9,28 @@ class PolyTreeNode
     end
 
     def parent=(node)
+        if self.parent != nil
+            old_parent = self.parent
+            old_parent.children.delete(self)
+        end
         @parent = node
         if node != nil
             if !node.children.include?(self)  
             node.children << self
              end
         end
+    end
+
+    def add_child(node)
+        node.parent=(self)
+    end
+
+    def remove_child(node)
+        if node.parent == nil
+            raise_error 'the node has no parent/node is not a child' 
+        end 
+        node.parent=(nil)
+
     end
 
     
